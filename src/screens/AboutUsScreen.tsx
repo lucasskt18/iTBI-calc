6a7d136c94fc78bb89e18b140d117c967d622439
Arthur Moreira <arthurmoreira3254@gmail.com>
2025-04-18 20:30:31 -0300

    add video for AboutUsScreen

--- src/screens/AboutUsScreen.tsx
+++ src/screens/AboutUsScreen.tsx
@@ -1,45 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, SafeAreaView, StatusBar } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+import { ActivityIndicator } from 'react-native';
 import { Icon } from '@rneui/themed';
 import BackButton from '../components/BackButton';
 
 export default function AboutUsScreen() {
+    const [videoLoaded, setVideoLoaded] = useState<boolean>(false);
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle="light-content" backgroundColor="#1A1A2E" />
             <BackButton />
             <ScrollView style={styles.scrollView}>
                 <View style={styles.header}>
-                    <Icon name="info-circle" type="font-awesome-5" color="#11998e" size={40} />
+                    <Icon name="info-circle" type="font-awesome-5" color="#414692" size={50} />
                     <Text style={styles.title}>Sobre Nós</Text>
                 </View>
 
                 <View style={styles.section}>
                     <Text style={styles.sectionTitle}>Sobre o Aplicativo</Text>
                     <Text style={styles.text}>
-                        O iTBI-calc é uma aplicação desenvolvida para facilitar o cálculo e gestão do Imposto sobre Transmissão de Bens Imóveis (ITBI).
-                        Com uma interface intuitiva e recursos avançados, o aplicativo auxilia proprietários e profissionais do setor imobiliário
-                        a gerenciarem seus imóveis e cálculos tributários de forma eficiente.
+                        O iTBI-calc é um aplicativo criado para simplificar o cálculo e a gestão do Imposto sobre Transmissão de Bens Imóveis (ITBI).
+                        Com uma interface intuitiva e funcionalidades inteligentes, ele oferece uma solução prática tanto para proprietários quanto para profissionais do mercado imobiliário, tornando a administração de imóveis e tributos muito mais eficiente.
                     </Text>
+
+                    <Video
+                        source={require('../../assets/videoApresentacao.mp4')}
+                        style={styles.video}
+                        useNativeControls
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping
+                        onLoadStart={() => setVideoLoaded(false)}
+                        onLoad={() => setVideoLoaded(true)}
+                        isMuted={false}
+                    />
+
+                    {!videoLoaded && (
+                        <ActivityIndicator size="large" color="#8F94FB" style={styles.loadingIndicator} />
+                    )}
+
+
                 </View>
 
                 <View style={styles.section}>
                     <Text style={styles.sectionTitle}>Nossa Missão</Text>
                     <Text style={styles.text}>
-                        Nossa missão é simplificar o processo de cálculo e gestão do ITBI, tornando-o mais acessível e transparente para todos os usuários.
-                        Buscamos constantemente melhorar a experiência do usuário e adicionar novas funcionalidades que agreguem valor ao seu dia a dia.
+                        Nossa missão é simplificar o cálculo e a gestão do ITBI, tornando esse processo mais acessível, transparente e prático para todos os usuários.
+                        Estamos sempre em busca de aprimorar a experiência dentro do app, adicionando novas funcionalidades que realmente façam a diferença no seu dia a dia.
                     </Text>
                 </View>
 
                 <View style={styles.section}>
                     <Text style={styles.sectionTitle}>Desenvolvedores</Text>
                     <View style={styles.teamMember}>
-                        <Icon name="user" type="font-awesome-5" color="#11998e" size={24} />
+                        <Icon name="user" type="font-awesome-5" color="#414692" size={24} />
                         <Text style={styles.teamMemberName}>Equipe de Desenvolvimento</Text>
                     </View>
                     <Text style={styles.text}>
-                        Desenvolvido por uma equipe dedicada de profissionais apaixonados por tecnologia e inovação,
-                        comprometidos em entregar a melhor solução para nossos usuários.
+                    Somos um grupo de alunos dedicados, interessados em tecnologia e focados em criar soluções que proporcionem uma boa experiência para os nossos usuários.
                     </Text>
                 </View>
 
@@ -77,15 +95,16 @@ const styles = StyleSheet.create({
     section: {
         padding: 20,
         marginBottom: 20,
+        position: 'relative',
     },
     sectionTitle: {
-        fontSize: 20,
+        fontSize: 22,
         fontWeight: 'bold',
-        color: '#11998e',
+        color: '#8F94FB',
         marginBottom: 12,
     },
     text: {
-        fontSize: 16,
+        fontSize: 17,
         color: '#FFF',
         lineHeight: 24,
     },
@@ -95,9 +114,27 @@ const styles = StyleSheet.create({
         marginBottom: 12,
     },
     teamMemberName: {
-        fontSize: 18,
+        fontSize: 19,
         fontWeight: '600',
         color: '#FFF',
         marginLeft: 12,
+    },
+
+    video: {
+        width: '100%',
+        height: 200,
+        marginTop: 20,
+        borderRadius: 8,
+    },
+
+    loadingIndicator: {
+        position: 'absolute', 
+        top: 0,  
+        left: 0, 
+        right: 0,
+        bottom: 0, 
+        marginTop: 250,
+        justifyContent: 'center', 
+        alignItems: 'center', 
     },
 }); 
